68f1905c3ad410e6343ea6c33c9aaf161595259a
CodeBrauer <gabrielwanzek@ymail.com>
2014-12-03 13:57:05 +0100

    Init v1

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// https://developer.chrome.com/devtools/docs/console#styling-console-output-with-css
+console.log('%c Suprise! ', 'background:crimson;color:#fff;font:20px/50px Helvetica Neue;padding 10px 20px;');
+console.log('%c     ', 'font-size:100px; background: url(http://cdn.nyanit.com/nyan2.gif)');
